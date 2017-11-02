921a1fb018bfe66faaff9574ebf30ad286ffe0c7
Patchariya <pimmiesz.p@gmail.com>
2017-11-02 15:04:13 +0700

    update token

--- hibot.py
+++ hibot.py
@@ -19,6 +19,7 @@ handler = WebhookHandler('ae05c21de4d24de723d09aa4a64eb206')
 def index():
     return "hi big"
 
+"""
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -35,7 +36,29 @@ def callback():
         abort(400)
 
     return 'OK'
+"""
 
+@app.route("/callback", methods=['GET','POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    if request.method == 'POST':
+        return request.data
+    elif request.method == 'GET':
+        return request.gata
+
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
